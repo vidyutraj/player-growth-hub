13abc5114b837dafd9fe6794265dae462f05b071
vidyutraj <vidyut.rajagopal2006@gmail.com>
2026-03-31 17:06:21 -0400

    fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,26 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import { LabelsConfigProvider } from "@/context/LabelsConfigContext";
+import PlayerDevelopment from "./pages/PlayerDevelopment";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
+    <LabelsConfigProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/" element={<PlayerDevelopment />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+        </BrowserRouter>
+      </TooltipProvider>
+    </LabelsConfigProvider>
   </QueryClientProvider>
 );
 
